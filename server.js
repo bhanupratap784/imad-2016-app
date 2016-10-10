14c1473a21c629ec5626314378c8c11154980000
bhanupratap784 <bhanupratap784@gmail.com>
2016-10-10 21:59:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone= {
+    title: 'ARticle-one',
+    heading: 'Aticle one',
+    date: 'october 12,2016',
+    content : 'I AM BHANU PRAtAP SINGH'
+};
+function createtemplate(data) {
+    var title= data.title;
+    var date= data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate={
+    <html>
+    <head>
+        <title>
+            ${title}
+            
+        </title>
+<link href="ui/style.css" rel="stylesheet" />
+    </head>
+
+<body>
+    <div class="container">
+        <div>
+        <a href="/ui/index.html">HOME</a>
+    </div>
+    <h1>
+        ${heading}
+        
+    </h1>
+    <div>
+        ${date}
+    </div>
+    <div>
+      ${content}
+    </div>
+    </div>
+    
+</body>
+</html>
+};
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +61,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 
 
