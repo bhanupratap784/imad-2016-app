d088b3cf429cbc5c47a26b1c184f8fab95b5b431
bhanupratap784 <bhanupratap784@gmail.com>
2016-10-12 22:19:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,11 @@ console.log('Loaded!');
 
 var img=document.getElementById('bhanu');
 img.onclick=function(){
+    var marginLeft=0;
+    function moveRight(){
+        marginLeft = marginLeft+10;
+        img.style.margin=marginLeft+'px';
+    }
+    var interval=setInterval(moveRight,100);
     img.style.marginleft='10px';
 };
