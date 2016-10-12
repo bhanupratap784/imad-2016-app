90bcbaea5ee8b000b1ade6a3400f0056bc59cbb2
bhanupratap784 <bhanupratap784@gmail.com>
2016-10-12 22:06:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,63 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles ={ 
-articleone:{
-    title: 'ARticle-one',
-    heading: 'Aticle one',
-    date: 'october 12,2016',
-    content : `I AM BHANU PRAtAP SINGH`
-},
-articletwo:{
-     title: 'ARticle-2',
-    heading: 'Aticle 2',
-    date: 'october 16,2016',
-    content : `I AM BHANU PRAtAP SINGH`
-},
-
-articlethree:{
-     title: 'ARticle-3',
-    heading: 'Aticle 2',
-    date: 'october 15,2016',
-    content : `I AM BHANU PRAtAP SINGH`
-}
-
-};
-function createtemplate(data) {
-    var title= data.title;
-    var date= data.date;
-    var heading=data.heading;
-    var content=data.content;
-var htmltemplate=`
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-<link href="ui/style.css" rel="stylesheet" />
-    </head>
-
-<body>
-    <div class="container">
-        <div>
-        <a href="/ui/index.html">HOME</a>
-    </div>
-    <h1>
-        ${heading}
-        
-    </h1>
-    <div>
-        ${date}
-    </div>
-    <div>
-        ${content}
-    </div>
-    </div>
-    
-</body></html>
-`;
-return createtemplate;
-}
 
 
 app.get('/', function (req, res) {
