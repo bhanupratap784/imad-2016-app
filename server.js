4e45840c423df145cc1e17fe39c6fff4407e1042
bhanupratap784 <bhanupratap784@gmail.com>
2016-11-01 19:59:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/info', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'info.html'));
+});
 
 
 
